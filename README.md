4060736babf9ab6bb8e5788a739b880174a35638
maluvicentini <vicentinimalu1@gmail.com>
2026-06-15 09:48:38 -0300

    feat: lista

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto 
+
+Aula de React: https://www.youtube.com/watch?v=6hiqVVCsA_I
+
+Aula de Tailwind: https://www.youtube.com/watch?v=uOnYYzl7hpI&t=29s
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import './App.css'
+import ItemLista from "./ItemLista"
 
 function App() {
+let listaMercado = [
+  "Banana", 
+  "Maça", 
+  "Carne",
+]
+
+  const adicionarElementoNaLista = () => {
+    const novaLista = [...listaMercado]
+    novaLista.push('Novo item');
+
+    listaMercado = novaLista;
+    console.log(novaLista);
+  }
 
   return (
     <>
+      <h1>Lista de Mercado</h1>
+      <input type="text" placeholder='Digite um item' />
+      <button onClick={() => adicionarElementoNaLista()} >Adicionar</button>
+
+      <ul>
+        {listaMercado.map((itemLista, index) => (
+          <ItemLista key={index} itemLista={itemLista}/>
+        ))}
+      </ul>
+
+      
     </>
   )
 }
